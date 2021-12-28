142415d5785250b0e206ccbce9fbca431326273b
rayan <rayanbak257@gmail.com>
2021-12-28 19:07:36 +0100

    commit

---
+++ function.py
@@ -0,0 +1,38 @@
+import cv2
+
+carplate_haar_cascade = cv2.CascadeClassifier("/home/ros/opencv_build/opencv/data/haarcascades/haarcascade_russian_plate_number.xml")
+class plate:
+    def detect(image):
+        carplate_overlay = image.copy() 
+        carplate_rects = carplate_haar_cascade.detectMultiScale(carplate_overlay,scaleFactor=1.1, minNeighbors=3)
+        for x,y,w,h in carplate_rects: 
+            cv2.rectangle(carplate_overlay, (x,y), (x+w,y+h), (255,0,0), 5)        
+        return carplate_overlay
+
+    def extract(image):
+        carplate_rects = carplate_haar_cascade.detectMultiScale(image,scaleFactor=1.1, minNeighbors=5)
+        for x,y,w,h in carplate_rects: 
+            carplate_img = image[y+10:y+h-10 ,x+10:x+w-15]        
+        return carplate_img
+
+    def enlarge_img(image, scale_percent):
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+        return resized_image
+
+    def region(text: str):
+        regions={"1": "ADRAR","2":"CHLEF","3":"LAGHOUAT","4":"OUM BOUAGHI","5":"BATNA","6":"BEJAIA",
+                "7":"BISKRA","8":"BECHAR","9":"BLIDA","10":"BOUIRA","11":"TAMANRASSET",
+                "12":"TEBESSA","13":"TLEMCEN","14":"TIARET","15":"TIZI OUZOU","16":"ALGER",
+                "17":"DJELFA","18":"JIJEL","19":"SETIF","20":"SAIDA","21":"SKIKDA","22":"SIDI BEL ABBES","23":"ANNABA","24":"GUELMA",
+                "25":"CONSTANTINE","26":"MEDEA","27":"MOSTAGANEM","28":"M'SILA","29":"MASCARA","30":"OUARGLA","31":"ORAN","32":"EL BAYDH",
+                "33":"ILLIZI","34":"BORDJ BOU ARRERIDJ","35":"BOUMERDES","36":"EL TAREF","37":"TINDOUF","38":"TISSEMSILT","39":"EL OUED",
+                "40":"KHENCHLA","41":"SOUK AHRASS","42":"TIPAZA","43":"MILA","44":"AÏN DEFLA","45":"NÂAMA","46":"AÏN TEMOUCHENT","47":"GHARDAÏA","48":"RELIZANE"}
+        return regions[text]
+
+    def type_vehicule(text: str):
+        types={"1": "vehicule particulier", "2": "camion", "3": "camionette", "4": "autobus", 
+                "5": "tracteur routier", "6": "tracteur", "7": "vehicule special", "8": "remorque", "9": "moto"}
+        return types[text]
---
+++ plate_test.py
@@ -0,0 +1,39 @@
+import pytesseract
+import cv2
+from pytesseract import Output
+from function import *
+
+img = cv2.imread("plate5.jpg")
+cv2.imshow('img', img)
+
+carplate_img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+detected_carplate_img = plate.detect(carplate_img_rgb)
+cv2.imshow('detected_carplate_img', detected_carplate_img)
+carplate_extract_img = plate.extract(carplate_img_rgb)
+carplate_extract_img = plate.enlarge_img(carplate_extract_img, 120)
+
+carplate_extract_img_gray = cv2.cvtColor(carplate_extract_img, cv2.COLOR_RGB2GRAY)
+
+thresh = cv2.threshold(carplate_extract_img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+d = pytesseract.image_to_data(thresh, config=f'--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789', output_type=Output.DICT)
+
+n_boxes = len(d['level'])
+for i in range(n_boxes):
+    (text,x,y,w,h) = (d['text'][i],d['left'][i],d['top'][i],d['width'][i],d['height'][i])
+    cv2.rectangle(carplate_extract_img, (x,y), (x+w,y+h) , (0,255,0), 2)
+text=d['text'][-1]
+info=[f"numero de plaque {text}", f"wilaya de {plate.region(text[-2:])}", f"annee 20{text[6:8]}", f"type de vehicule: {plate.type_vehicule(text[5:6])}"]
+
+
+cv2.rectangle(img, (20, 20), (650, 150), (59, 57, 56), -1)
+cv2.putText(img, info[0], (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,51,255), 2, cv2.LINE_AA)
+cv2.putText(img, info[1], (50,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,51,255), 2, cv2.LINE_AA) 
+cv2.putText(img, info[2], (50,110), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,51,255), 2, cv2.LINE_AA) 
+cv2.putText(img, info[3], (50,140), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,51,255), 2, cv2.LINE_AA)  
+cv2.imshow('img', img)
+cv2.imshow("detected_carplate_img", detected_carplate_img)
+cv2.imshow("carplate_extract_img", carplate_extract_img)
+cv2.imshow('threshold', thresh)
+cv2.waitKey(0)
